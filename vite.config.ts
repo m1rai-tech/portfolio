ea810119c388bb063c3cbd4a4215502dffcf4681
Mirai <danylo.i.storozhuk@ukd.edu.ua>
2026-05-30 09:11:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -35,3 +35,7 @@ export default defineConfig({
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
+export default defineConfig({
+  base: '/portfolio/',
+  // ...
+})
